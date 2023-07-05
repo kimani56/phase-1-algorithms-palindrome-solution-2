7fd92c4df056af162001e11fff2e7ad97fcbf8ba
gift kimani <gift.kimani@students.moringaschool.com>
2023-07-05 18:14:39 +0300

    complete

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+
+for(i = 0; i < word.length /2; i++) {
+const j = word.length -1 -i;
+if(word[i] !== word[j]){
+  return false;
+} 
 }
+return true;
+}
+
 
 /* 
   Add your pseudocode here
+  iterate from the beginning of the string to the middle of the string
+  compare the letter we're iterating over to the corresponding letter at the end of the string
+    if the letters don't match, return false
+
+if we reach the middle, and all the letters match, return true
 */
 
 /*
